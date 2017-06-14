b8fa33137471e1f9508ad9294ec58e9d097cf758
TedaLIEz <aliezted@gmail.com>
2017-06-14 20:53:08 +0800

    feat: main activity layout

--- app/src/main/java/com/hustunique/androidassistant/MainActivity.java
+++ app/src/main/java/com/hustunique/androidassistant/MainActivity.java
@@ -25,23 +25,11 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
-import com.hustunique.androidassistant.model.AppInfo;
-import com.hustunique.androidassistant.receiver.PowerReceiver;
-import com.hustunique.androidassistant.receiver.PowerReceiver.BatteryCallback;
-import com.hustunique.androidassistant.service.MyPowerManager;
-import com.hustunique.androidassistant.util.LogUtil;
-import java.util.List;
-import android.util.Log;
-import android.widget.TextView;
-
 import com.hustunique.androidassistant.db.BlackList;
 import com.hustunique.androidassistant.receiver.PhoneCallReceiver;
 import com.hustunique.androidassistant.receiver.PowerReceiver;
 import com.hustunique.androidassistant.receiver.PowerReceiver.BatteryCallback;
+import com.hustunique.androidassistant.service.MyPowerManager;
 import com.hustunique.androidassistant.util.LogUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private PowerReceiver mPowerReceiver;
     private PhoneCallReceiver mCallReceiver;
-    private TextView mTextView;
-    private Button mBtnKill;
     private MyPowerManager mPowerManager;
     private final int MY_PERMISSIONS_REQUEST_PHONE_CALL = 1;
 
@@ -58,22 +44,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.battery);
-        mBtnKill = (Button) findViewById(R.id.kill_proc);
         mPowerManager = new MyPowerManager(this);
-        mBtnKill.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                long mem = mPowerManager.getAvailableMemory();
-                LogUtil.d(TAG, "availableMegs in mb " + mem);
-                List<AppInfo> list = mPowerManager.getRecentUsedApps();
-                mPowerManager.killProcesses(list);
-            }
-        });
+
         mPowerReceiver = new PowerReceiver(new BatteryCallback() {
             @Override
             public void onUpdated(int pct) {
-                mTextView.setText("battery pct: " + pct + "%");
+                LogUtil.d(TAG, "battery pct: " + pct + "%");
             }
         });
 
