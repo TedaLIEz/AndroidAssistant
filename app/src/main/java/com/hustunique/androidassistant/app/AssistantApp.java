4176fa3b19b0d9865bc0beb36de37f723a0c93a1
CoXier <justcoxier@gmail.com>
2017-06-15 09:13:28 +0800

    fix : fix mobile data when device boot

--- app/src/main/java/com/hustunique/androidassistant/app/AssistantApp.java
+++ app/src/main/java/com/hustunique/androidassistant/app/AssistantApp.java
@@ -1,15 +1,29 @@
 package com.hustunique.androidassistant.app;
 
 import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.hustunique.androidassistant.service.MobileDataService;
+import com.hustunique.androidassistant.util.LogUtil;
+import com.hustunique.androidassistant.util.Util;
 
 /**
  * Created by CoXier on 17-6-13.
  */
 
 public class AssistantApp extends Application {
+    private static final String TAG = "AssistantApp";
+
     @Override
     public void onCreate() {
         super.onCreate();
-
+        LogUtil.d(TAG, "Application create");
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        LogUtil.d(TAG, "Mobile data: " + sharedPreferences.getLong("mobileData", -1));
+        if (Util.isMobileType(this)) {
+            Intent intent = new Intent(this, MobileDataService.class);
+            startService(intent);
+        }
     }
 }
